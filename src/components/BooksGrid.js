614760aba22e8765a99c44d87de5bf111cba4647
kevinthalmann <kevin.thalmann@gmail.com>
2018-12-07 15:29:20 -0200

    feat: show message if book shelf empty
    Signed-off-by: kevinthalmann <kevin.thalmann@gmail.com>

--- src/components/BooksGrid.js
+++ src/components/BooksGrid.js
@@ -4,6 +4,9 @@ import Book from './Book'
 const BooksGrid = ({ books, shelfChangeHandler }) => {
   return (
     <ol className="books-grid">
+      {books.length === 0 && (
+        <div style={{ textAlign: 'center' }}>No Books here</div>
+      )}
       {books.map(book => (
         <li key={book.id}>
           <Book book={book} shelfChangeHandler={shelfChangeHandler} />
